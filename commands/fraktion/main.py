0d967b5a9530650d1890cbaf932f3b477300b003
Maks891 <124050452+Maks891@users.noreply.github.com>
2024-06-27 16:47:07 +0300

    Update main.py

--- commands/fraktion/main.py
+++ commands/fraktion/main.py
@@ -26,8 +26,6 @@ titles = {
 
 # Обработка команды "Фракции"
 async def get_fractions(message: types.Message):
-    cursor.execute('SELECT name FROM fractions')
-    fractions = cursor.fetchall()
     fractions_list = '\n'.join([row[0] for row in fractions])
     await message.reply(f'Доступные фракции:\n{fractions_list}')
 # Обработка команды "Титулы"
