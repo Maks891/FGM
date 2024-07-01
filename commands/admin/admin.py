45e29977f9913a613f93b9ba678b51172e549a1d
Maks891 <124050452+Maks891@users.noreply.github.com>
2024-07-02 00:24:51 +0300

    Update admin.py

--- commands/admin/admin.py
+++ commands/admin/admin.py
@@ -77,7 +77,7 @@ async def admin_menu(message: types.Message):
         keyboard=[
             [types.KeyboardButton(text='📍 Рассылка'), types.KeyboardButton(text='🕹 Управление')],
             [types.KeyboardButton(text='✨ Промокоды'), types.KeyboardButton(text='📥 Выгрузка')],
-            [types.KeyboardButton(text='⚙️ Изменить текст рекламы')]
+            [types.KeyboardButton(text='⚙️ Изменить текст рекламы')], types.KeyboardButton(text='❗ Сбросить время лимитов ❗')
         ],
         resize_keyboard=True
     )
